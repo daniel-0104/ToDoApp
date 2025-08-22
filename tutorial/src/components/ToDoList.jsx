c411572cacf3dcc7ae7d65921e75c3ec02628fa0
Daniel <122777459+daniel-0104@users.noreply.github.com>
2025-08-22 19:32:19 +0630

    22 last commit

--- tutorial/src/components/ToDoList.jsx
+++ tutorial/src/components/ToDoList.jsx
@@ -1,9 +1,10 @@
 import ToDoItem from "./ToDoItem";
 import styles from "./todolist.module.css";
 export default function ToDoList({ todoList, setTodoList }) {
+  const sortedTodo = todoList.slice().sort((a,b)=>Number(a.done)-Number(b.done));
   return (
     <div className={styles.list}>
-      {todoList.map((item) => (
+      {sortedTodo.map((item) => (
         <ToDoItem
           key={item.name}
           item={item}
